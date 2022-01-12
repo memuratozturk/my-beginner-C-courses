8a12c2aca891b2228aaa7f76639b65a1847a008b
murat öztürk <71570762+memuratozturk@users.noreply.github.com>
2022-01-13 01:13:10 +0300

    Find ASCII
    program to find the ASCII equivalent of the entered character

---
+++ Fınd_ASCII.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    char ascchar;
+    printf("Enter characters to find the ASCII equivalent: \n");
+    scanf("%c", &ascchar);
+    printf("\n The ASCII equivalent of the entered character: %d \n ", ascchar);
+
+
+}
