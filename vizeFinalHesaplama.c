c751a2f4f08520192022df3d831e6b83c960d259
murat öztürk <71570762+memuratozturk@users.noreply.github.com>
2022-01-14 17:45:58 +0300

    Update vizeFinalHesaplama.c

--- vizeFinalHesaplama.c
+++ vizeFinalHesaplama.c
@@ -7,7 +7,7 @@ dönem not ortalamasını hesaplayan C programı.
 not: vize sınavının katkısı %40 final sınavının katkısı %60 olsun. 
 */
 int vize, final;
-int ort;
+float ort;
 printf("vize notunuzu giriniz: \n");
 scanf("%d", &vize);
 printf("vizeniz %d", vize);
@@ -15,8 +15,7 @@ printf("\n final notunuzu giriniz : ");
 scanf("%d", &final);
 printf("final notunuz : %d", final);
 ort = (vize * 0.4) + (final * 0.6);
-printf("\n ortalamanız %d \n", ort);
- // buradan sonrasını ben ekledim.
+printf("\n ortalamanız %f \n", ort);
 if(ort>80) printf("AA");
 else if (ort>75) printf("AB");
 else if (ort>60) printf("BB");
